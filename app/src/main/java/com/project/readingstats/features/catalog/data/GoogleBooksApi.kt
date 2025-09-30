d6f0f82c9917fab16d106f349bc3dd09ce523c4b
Chris0451 <dcchris56@gmail.com>
2025-09-30 20:47:03 +0200

    Update GoogleBooksApi.kt

--- app/src/main/java/com/project/readingstats/features/catalog/data/GoogleBooksApi.kt
+++ app/src/main/java/com/project/readingstats/features/catalog/data/GoogleBooksApi.kt
@@ -3,7 +3,7 @@ package com.project.readingstats.features.catalog.data
 import androidx.compose.ui.input.key.Key
 import retrofit2.http.GET
 import retrofit2.http.Query
-import com.project.readingstats.features.catalog.BuildConfig
+//import com.project.readingstats.features.catalog.BuildConfig
 
 interface GoogleBooksApi {
     @GET("volumes")
@@ -14,6 +14,6 @@ interface GoogleBooksApi {
         @Query("orderBy") orderBy: String? = "relevance",
         @Query("langRestrict") langRestrict: String? = "it",
         @Query("projection") projection: String? = "lite",
-        @Query("key") key: String = BuildConfig.GOOGLE_BOOKS_API_KEY
+        //@Query("key") key: String = BuildConfig.GOOGLE_BOOKS_API_KEY
     )
 }
